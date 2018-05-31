f379747a2bdae879fcae48d9c9d1dcd83ea5f8aa
Towa <towa@macbook-pro-de-towa.local>
2018-05-31 15:14:04 -0700

    Initial Commit

---
+++ task-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  task-app
+//
+//  Created by Towa on 31/05/18.
+//  Copyright © 2018 Towa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

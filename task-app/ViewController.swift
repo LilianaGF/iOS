1ce28e9d75c9af4de6ef90f952ad2943cb3f8d2b
Liliana Gutiérrrez <lilianaf.750@gmail.com>
2018-06-07 20:06:23 -0500

    Ejercicios 2 y 3. Ciclo de vida y nueva scene

--- task-app/ViewController.swift
+++ task-app/ViewController.swift
@@ -1,4 +1,4 @@
-//
+///
 //  ViewController.swift
 //  task-app
 //
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TaskDoneText: UITextView!
+    @IBOutlet weak var TaskToDoText: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("on viewDidLoad() method");
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        print("on viewWillAppear() method");
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("on viewDidAppear() method");
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("on viewWillDisappear() method");
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("on viewDidDisappear() method");
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func ShowNewTaskForm(_ sender: Any) {
+        print("New Button clicked");
+        TaskDoneText.text = "JIJI";
+    }
+    
 }
 
